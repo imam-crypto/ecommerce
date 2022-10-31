ac0d6e402240558e0e7c30e29a44b9ed1e78c3a2
imam-crypto <purnamaimam0@gmail.com>
2022-10-31 11:00:31 +0700

    add category

--- request/user.go
+++ request/user.go
@@ -10,3 +10,13 @@ type LoginUserInput struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
+
+type UpdateUserInput struct {
+	Email      string `json:"email"`
+	Address    string `json:"address"`
+	PostalCode string `json:"postal_code"`
+	City       string `json:"city"`
+	Province   string `json:"province"`
+	Gender     string `json:"gender"`
+	Name       string `json:"name"`
+}
